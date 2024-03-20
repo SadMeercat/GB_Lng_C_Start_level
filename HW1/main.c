18ab5e1e1a8e587067f9e02cf4581243ffd1eac7
SadMeercat <igor9823@gmail.com>
2024-03-20 20:57:34 +0300

    Finish hw 1

---
+++ HW1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello world!\n");
+
+    printf("Let's\n\tgo\n\t\tto walk\n");
+
+    printf("   *\n");
+    printf("  ***\n");
+    printf("*******\n");
+    printf(" HH HH\n");
+    printf(" ZZZZZ\n");
+}
